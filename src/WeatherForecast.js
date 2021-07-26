3b32a64a4ad91e9f4abb15b3bd641a470561677e
schawanji <35290804+schawanji@users.noreply.github.com>
2021-07-26 23:37:27 +0200

    Updated weather forecast

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -7,32 +7,13 @@ function WeatherForecast(props) {
   let [response, setResponse] = useState([]);
   let days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"];
   function getWeatherData(response) {
-    console.log(response.data.daily);
+    console.log(response.data.daily[0]);
   }
 
   let url = `https://api.openweathermap.org/data/2.5/onecall?lat=${props.weather.lat}&lon=${props.weather.lon}&exclude=hourly&appid=e4dfdc1dfbd9af8701deee7d18b22e9b&units=metric`;
 
   axios.get(url).then(getWeatherData);
 
-  return (
-    <div className="row">
-      
-      <div className="col-sm-2">
-        <div className="weatherforecast">
-          <div className="forecast-day"></div>
-          <div className="forecast-icon">
-            <ReactAnimatedWeather
-              icon={"CLEAR_DAY"}
-              color={""}
-              size={38}
-              animate={true}
-            />
-          </div>
-          <div className="forecast-temperature">°</div>
-        </div>
-      </div>
-      
-    </div>
-  );
+  return "";
 }
 export default WeatherForecast;
